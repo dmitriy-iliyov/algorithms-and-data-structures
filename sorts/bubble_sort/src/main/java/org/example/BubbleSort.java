348e64c62f86178fb54fe36c493b205f6d11ab5b
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-31 13:20:12 +0200

    add bubble_sort

---
+++ sorts/bubble_sort/src/main/java/org/example/BubbleSort.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class BubbleSort {
+    public static int[] bubble_sort(int[] a){
+        for( int i=0; i < a.length; i++){
+            for(int j=a.length-1; j > i; j-- ){
+                if ( a[j-1] > a[j]){
+                    int tmp=a[j-1];
+                    a[j-1]=a[j];
+                    a[j]=tmp;
+                }
+            }
+        }
+        return a;
+    }
+
+}
